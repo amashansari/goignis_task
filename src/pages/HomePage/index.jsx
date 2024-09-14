9b8012dfac9f39579b62315fec5004d384bed693
Rameez Khan <115420801+R4MEEZ@users.noreply.github.com>
2024-09-14 15:09:02 +0530

    project start

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <div className="homepage"></div>
+    // <div className="homepage-container">
+    //   <div className="custom-nav">
+    //     <nav className="navbar navbar-expand-lg navbar-light">
+    //       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    //         <div id="pq-menu-contain" className="pq-menu-contain">
+    //           <ul id="pq-main-menu" className="navbar-nav ml-auto">
+    //             <li className="menu-item menu-item-has-children current-menu-item">
+    //               <Link href="/">Home</Link>
+    //             </li>
+    //           </ul>
+    //         </div>
+    //       </div>
+    //     </nav>
+    //   </div>
+    // </div>
+  );
+};
+
+export default HomePage;
