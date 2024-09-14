235b26575ca4475ef7038d201e68210395fd383f
Amash10 <amashansari74@gmail.com>
2024-09-14 17:08:42 +0530

    pull arif

--- src/pages/ServicePage/index.js
+++ src/pages/ServicePage/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 const ServicePage = () => {
   return (
     <div>
-      Services
     </div>
   )
 }
--- src/pages/WhyPage/index.js
+++ src/pages/WhyPage/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 const WhyPage = () => {
   return (
     <div>
-      Why Us
     </div>
   )
 }
