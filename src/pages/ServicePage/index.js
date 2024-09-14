6fdf2690645405b8dd14ec77c7d6f331971a1b51
aarifansari99 <aarif454b@gmail.com>
2024-09-14 15:09:34 +0530

    project pull

--- src/pages/ServicePage/index.js
+++ src/pages/ServicePage/index.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import ServiceImg from "../../assets/images/service 1.jpg";
 
 const ServicePage = () => {
   return (
-    <div>
-      Services
-    </div>
-  )
-}
+    <>
+      <div className="service-container">
+        <div className="pq-service-box-2-list">
+          <div className="item">
+            <div className="pq-service-box pq-style-2">
+              <div className="pq-service-number">01</div>
+              <div className="pq-service-box-title">
+                <h5 className="pq-service-title">
+                  {" "}
+                  Search Engine Optimization
+                </h5>
+              </div>
+              <div className="pq-service-description">
+                IT service providers may offer custom software development
+                Planning and sketching Modern designtestlly practically point of
+                view.
+                <ul className="pq-service-list">
+                  <li>Mobile &amp; Web Design</li>
+                  <li>Interation Design</li>
+                  <li>UX Research &amp; Plan</li>
+                </ul>
+              </div>
+              <div className="pq-service-img">
+                <img src={ServiceImg} decoding="async" alt="service-image" />
+                {/* <img decoding="async" src="images/service/list/1.jpg" alt="service-image"> */}
+              </div>
+              <div className="pq-btn-container">
+                <a
+                  className="pq-button pq-button-flat"
+                  href="software-development.html"
+                >
+                  <span className="text">read more</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ServicePage
+export default ServicePage;
