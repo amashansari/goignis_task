db3fba36455f0afa9d16bc5afad6c0a8ff21a10f
aarifansari99 <aarif454b@gmail.com>
2024-09-14 17:08:06 +0530

    project pull

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ import "./assets/styles/global.css"
 function App() {
   return (
  <div className='App'>
-  <HomePage/>
-  <AboutPage/>
+  {/* <HomePage/> */}
+  {/* <AboutPage/> */}
   <ServicePage/>
-  <WhyPage/>
-  <WorkingProcess/>
-  <ContactPage/>
+  {/* <WhyPage/> */}
+  {/* <WorkingProcess/> */}
+  {/* <ContactPage/> */}
    </div>
   );
 }
--- src/pages/ServicePage/index.js
+++ src/pages/ServicePage/index.js
@@ -4,36 +4,150 @@ import ServiceImg from "../../assets/images/service 1.jpg";
 const ServicePage = () => {
   return (
     <>
-      <div className="service-container">
-        <div className="pq-service-box-2-list">
+      <div className="service-page-container">
+        <div class="service-title style-1 text-center">
+          <span class="service-sub-title">Our services</span>
+          <h5 class="service-main-title">
+            We Help You Grow Your Business Expntially
+          </h5>
+        </div>
+        <div className="service-box-list">
           <div className="item">
-            <div className="pq-service-box pq-style-2">
-              <div className="pq-service-number">01</div>
-              <div className="pq-service-box-title">
-                <h5 className="pq-service-title">
-                  {" "}
-                  Search Engine Optimization
-                </h5>
+            <div className="service-box style-2">
+              <div className="service-number">01</div>
+              <div className="service-box-title">
+                <h5 className="service-title"> FIRE ALARM ENGINEERING</h5>
               </div>
-              <div className="pq-service-description">
-                IT service providers may offer custom software development
-                Planning and sketching Modern designtestlly practically point of
-                view.
-                <ul className="pq-service-list">
-                  <li>Mobile &amp; Web Design</li>
-                  <li>Interation Design</li>
-                  <li>UX Research &amp; Plan</li>
+              <div className="service-description">
+                At GoIGNIS, we understand the critical role that a well-designed
+                fire alarm system plays in protecting life and property....
+                <ul className="service-list">
+                  <li>Full Coverage Design</li>
+                  <li>Quick Detection & Alert</li>
+                  <li>Custom Systems</li>
                 </ul>
               </div>
-              <div className="pq-service-img">
+              <div className="service-img">
                 <img src={ServiceImg} decoding="async" alt="service-image" />
                 {/* <img decoding="async" src="images/service/list/1.jpg" alt="service-image"> */}
               </div>
-              <div className="pq-btn-container">
-                <a
-                  className="pq-button pq-button-flat"
-                  href="software-development.html"
-                >
+              <div className="btn-container">
+                <a className="button button-flat">
+                  <span className="text">read more</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="service-box-list">
+          <div className="item">
+            <div className="service-box style-2">
+              <div className="service-number">02</div>
+              <div className="service-box-title">
+                <h5 className="service-title"> FIRE SPRINKLER ENGINEERING</h5>
+              </div>
+              <div className="service-description">
+                Our Fire Sprinkler Engineering team offers comprehensive design
+                services that comply with NFPA standards and relevant codes.
+                Using advanced software such as ......
+                <ul className="service-list">
+                  <li>NFPA Compliance</li>
+                  <li>Advanced Software</li>
+                  <li>BIM Integration</li>
+                </ul>
+              </div>
+              <div className="service-img">
+                <img src={ServiceImg} decoding="async" alt="service-image" />
+                {/* <img decoding="async" src="images/service/list/1.jpg" alt="service-image"> */}
+              </div>
+              <div className="btn-container">
+                <a className="button button-flat">
+                  <span className="text">read more</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="service-box-list">
+          <div className="item">
+            <div className="service-box style-2">
+              <div className="service-number">03</div>
+              <div className="service-box-title">
+                <h5 className="service-title"> ELECTRICAL ENGINEERING</h5>
+              </div>
+              <div className="service-description">
+                Our electrical engineering team at GoIGNIS understands the
+                ever-increasing complexity and functionality of building
+                systems,......
+                <ul className="service-list">
+                  <li>Quality Designs</li>
+                  <li>Expert Integration</li>
+                  <li>BIM Modeling</li>
+                </ul>
+              </div>
+              <div className="service-img">
+                <img src={ServiceImg} decoding="async" alt="service-image" />
+                {/* <img decoding="async" src="images/service/list/1.jpg" alt="service-image"> */}
+              </div>
+              <div className="btn-container">
+                <a className="button button-flat">
+                  <span className="text">read more</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="service-box-list">
+          <div className="item">
+            <div className="service-box style-2">
+              <div className="service-number">04</div>
+              <div className="service-box-title">
+                <h5 className="service-title"> HVAC ENGINEERING</h5>
+              </div>
+              <div className="service-description">
+                At GoIGNIS, we understand that heating, ventilation, and air
+                conditioning (HVAC) are crucial components in any building. We
+                have a team of....
+                <ul className="service-list">
+                  <li>Custom Design</li>
+                  <li>System Expertise</li>
+                  <li>Code Compliance</li>
+                </ul>
+              </div>
+              <div className="service-img">
+                <img src={ServiceImg} decoding="async" alt="service-image" />
+                {/* <img decoding="async" src="images/service/list/1.jpg" alt="service-image"> */}
+              </div>
+              <div className="btn-container">
+                <a className="button button-flat">
+                  <span className="text">read more</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="service-box-list">
+          <div className="item">
+            <div className="service-box style-2">
+              <div className="service-number">05</div>
+              <div className="service-box-title">
+                <h5 className="service-title"> ARCS ENGINEERING</h5>
+              </div>
+              <div className="service-description">
+                At GoIGNIS, we understand the importance of Auxiliary Radio
+                Communication Systems (ARCS) in high-rise buildings.............
+                <ul className="service-list">
+                  <li>Custom Design</li>
+                  <li>Continuous Operation</li>
+                  <li>Enhanced Communication</li>
+                </ul>
+              </div>
+              <div className="service-img">
+                <img src={ServiceImg} decoding="async" alt="service-image" />
+                {/* <img decoding="async" src="images/service/list/1.jpg" alt="service-image"> */}
+              </div>
+              <div className="btn-container">
+                <a className="button button-flat">
                   <span className="text">read more</span>
                 </a>
               </div>
